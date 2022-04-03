b9fd708457b84bc5eaacd189be3096ef4b1e4e41
fadhlurrahmanID <jedacoding@gmail.com>
2022-04-03 13:55:26 +0800

    Try

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        container: {
+            padding: {
+                lg: "9rem",
+                sm: "6rem",
+                md: "0rem",
+            },
+        },
+        extend: {
+            colors: {
+                primary: "#14b8a6",
+                secondary: "#64748b",
+                dark: "#0f172a",
+            },
+            screens: {
+                "2xl": "1320px",
+            },
+        },
+    },
+    plugins: [],
+};
